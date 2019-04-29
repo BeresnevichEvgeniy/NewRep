fc6d978326f9138f0f553a9aadb3fc5d0550c6bc
Evgeniy Beresnevich <beresnevichevgeniy@gmail.com>
2019-04-29 21:16:37 +0300

    3.1 Массивы, циклы

--- src/NewRep.java
+++ src/NewRep.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class NewRep {
     public static void main(String[] args) {
-        for (String s: args) {
-            System.out.println(s);
+        String John[] = {"And", "His", "Name", "John", "Cena"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи номер 1-6");
+        int k = scanner.nextInt();
+        switch (k) {
+            case 1:
+                for (int i = 0; i < John.length; i++)
+                    System.out.print(John[i] + " ");
+                break;
+            case 2:
+                int i = 0;
+                while (i < John.length){
+                    System.out.print(John[i] + " ");
+                i++;}
+                break;
+            case 3:
+                i = 0;
+                do {
+                    System.out.print(John[i] + " ");
+                    i++;
+                }
+                while (i<John.length);
+                break;
+            case 4:
+                for(String string : John){
+                    System.out.print(string + " ");
+                }
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                System.out.println("default");
+                break;
         }
-        System.out.println("Hello World");
     }
 }
